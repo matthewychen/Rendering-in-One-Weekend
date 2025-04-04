9e4077749f04db4b8e8f40bb0989c6651af187d4
Matthew Chen <matthewychen06@gmail.com>
2025-04-04 14:56:03 -0400

    refactored

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "headers/colour.h"
+#include "headers/vec3.h"
+#include <iostream>
+#include <fstream>
+
+int main() {
+
+    // Image
+
+    int image_width = 1024;
+    int image_height = 1024;
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_color = color(double(i)/(image_width-1), double(j)/(image_height-1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::clog << "\rDone.                 \n";
+}
