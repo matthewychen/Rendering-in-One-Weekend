91ac1e99c8905f4e9fd5979467dc3867f5ad0672
Matthew Chen <matthewychen06@gmail.com>
2025-04-05 13:04:45 -0400

    hittable list, discriminant math

---
+++ headers/hittable.h
@@ -0,0 +1,32 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.h"
+
+class hit_record {
+    public:
+      point3 p; //point of hit
+      vec3 normal; //normal at point p
+      double t; //position on ray where it hit
+      bool front_face; //whether it hit from the outside or the inside
+  
+      void set_face_normal(const ray& r, const vec3& outward_normal) {
+          // Sets the hit record normal vector.
+          // NOTE: the parameter `outward_normal` is assumed to have unit length.
+  
+          front_face = dot(r.direction(), outward_normal) < 0; //if direction is the same as the outward normal, it is on the inside
+          normal = front_face ? outward_normal : -outward_normal; //if the ray is coming from the inside, the normal should be pointing against the normal and back into the hittable object
+      }
+  };
+
+class hittable {//base class for objects that can be hittablew
+  public:
+    virtual ~hittable() = default; //generates destructor. allows overwriting in derived classes
+
+    virtual bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const = 0; //returns true if the object is hit, false if not
+    //ray_tmin makes sure no duplicate hits are counted, usually set to some small number
+    //ray_tmax maximum allowable t, allows hits behind other objects to be ignored
+    //=0 means its a pure virtual function and forces derived classes to implement it
+};
+
+#endif
