83065825b39eaf3dee3ef10e2a4c191f76c238a0
Matthew Chen <matthewychen06@gmail.com>
2025-04-07 17:43:16 -0400

    finished book 1

--- headers/camera.h
+++ headers/camera.h
@@ -8,10 +8,20 @@
 
 class camera {
   public: //default values
+  //RENDERING SETTINGS
     double aspect_ratio = 16.0 / 9.0;
     int image_width = 1700;
     int samples_per_pixel = 10; //random samples per pixel on screen
     int max_depth = 10; //most bounces before abort
+
+  //CAMERA PROPERTIES
+    double vfov = 90;  // Vertical view angle (field of view)
+    point3 lookfrom = point3(0,0,0);   // Point camera is looking from
+    point3 lookat   = point3(0,0,-1);  // Point camera is looking at
+    vec3   vup      = vec3(0,1,0);     // Camera-relative "up" direction. WORLD up
+
+    double defocus_angle = 0;  // Variation angle of rays through each pixel
+    double focus_dist = 10;    // Distance from camera lookfrom point to plane of perfect focus
 
     void render(const hittable& world) {//rendering block
         initialize();
@@ -38,30 +48,43 @@ class camera {
     point3 pixel00_loc;    
     vec3   pixel_delta_u;  
     vec3   pixel_delta_v;
+    vec3   u, v, w;              // Camera frame basis vectors
+    vec3   defocus_disk_u;       // Defocus disk horizontal radius
+    vec3   defocus_disk_v;       // Defocus disk vertical radius
+
       
 
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        //auto focal_length = (lookfrom - lookat).length(); //length from camera centre to target pos
+        auto theta = degrees_to_radians(vfov);
+        auto h = std::tan(theta/2);
+        auto viewport_height = 2 * h * focus_dist;
         auto viewport_width = viewport_height * (double(image_width)/image_height);
-        pixel_samples_scale = 1.0 / samples_per_pixel; //make each random sample contribute equally
-        
-        center = point3(0, 0, 0);
 
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        pixel_samples_scale = 1.0 / samples_per_pixel; //make each random sample contribute equally by adding all components into the colour result, and then dividing at the end
+        
+        center = lookfrom;
+
+        w = unit_vector(lookfrom - lookat); 
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
+        vec3 viewport_u = viewport_width * u;    // Vector across viewport horizontal edge
+        vec3 viewport_v = viewport_height * -v;  // Vector down viewport vertical edge
 
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
-        auto viewport_upper_left = center 
-                                - vec3(0, 0, focal_length) 
-                                - viewport_u/2 
-                                - viewport_v/2;
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+        // Calculate the camera defocus disk basis vectors.
+        auto defocus_radius = focus_dist * std::tan(degrees_to_radians(defocus_angle / 2));
+        defocus_disk_u = u * defocus_radius;
+        defocus_disk_v = v * defocus_radius;
     }
 
     colour ray_color(const ray& r, int depth, const hittable& world) const {
@@ -97,12 +120,16 @@ class camera {
                           + ((i + offset.x()) * pixel_delta_u)
                           + ((j + offset.y()) * pixel_delta_v);
 
-        auto ray_origin = center;
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         return ray(ray_origin, ray_direction);
     }
-
+    point3 defocus_disk_sample() const {
+        // Returns a random point in the camera defocus disk.
+        auto p = random_in_unit_disk();
+        return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
+    }
     vec3 sample_square() const {
         // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
--- headers/material.h
+++ headers/material.h
@@ -37,18 +37,59 @@ class lambertian : public material {//lambertian material with guarenteed scatte
 
   class metal : public material {
     public:
-      metal(const colour& albedo) : albedo(albedo) {}
+      metal(const colour& albedo, const double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {} //clamp fuzz to max 1
   
       bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scattered)
       const override {
-          vec3 reflected = reflect(r_in.direction(), rec.normal);
+          vec3 reflected = reflect(r_in.direction(), rec.normal); //reflected trajectory
+          reflected = unit_vector(reflected) + (fuzz * random_unit_vector()); //add fuzz
           scattered = ray(rec.p, reflected);
           attenuation = albedo;
-          return true;
+          //return true;
+          return (dot(scattered.direction(), rec.normal) > 0);//if not reflected just absorb
       }
   
     private:
       colour albedo;
+      double fuzz;
+  };
+
+  class dielectric : public material {
+    public:
+      dielectric(double refraction_index) : refraction_index(refraction_index) {}
+  
+      bool scatter(const ray& r_in, const hit_record& rec, colour& attenuation, ray& scattered)
+      const override {
+          attenuation = colour(1.0, 1.0, 1.0);//no attenuation at first
+          double ri = rec.front_face ? (1.0/refraction_index) : refraction_index;
+  
+          vec3 unit_direction = unit_vector(r_in.direction());
+          double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+          double sin_theta = std::sqrt(1.0 - cos_theta*cos_theta);
+  
+          bool cannot_refract = ri * sin_theta > 1.0;
+          vec3 direction;
+  
+          if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+              direction = reflect(unit_direction, rec.normal);
+          else
+              direction = refract(unit_direction, rec.normal, ri);
+  
+          scattered = ray(rec.p, direction);
+          return true;
+      }
+  
+    private:
+      // Refractive index in vacuum or air, or the ratio of the material's refractive index over
+      // the refractive index of the enclosing media
+      double refraction_index;
+
+      static double reflectance(double cosine, double refraction_index) {//given as a function of the incidence angle and the refraction index
+        // Use Schlick's approximation for reflectance.
+        auto r0 = (1 - refraction_index) / (1 + refraction_index);
+        r0 = r0*r0;
+        return r0 + (1-r0)*std::pow((1 - cosine),5);
+    }
   };
 
 #endif
